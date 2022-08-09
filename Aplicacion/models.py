27c2bb58eabd55e773a299a01d2858db3aa346c8
Sergio Córdoba <scordobamuriel@gmail.com>
2022-08-09 10:17:17 -0500

    creación del modelo

--- Aplicacion/models.py
+++ Aplicacion/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto (models.Model):
+    nombre = models.TextField(max_length=50)
+    price = models.FloatField()
+    rating = models.FloatField()
