57499672d70ffa434b73b8f1ca848385a74215a6
Sergio Córdoba <scordobamuriel@gmail.com>
2022-08-09 10:46:23 -0500

    cambio en atributo nombre

--- Aplicacion/models.py
+++ Aplicacion/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Producto (models.Model):
-    nombre = models.TextField(max_length=50)
+    nombre = models.CharField(max_length=50)
     price = models.FloatField()
     rating = models.FloatField()
