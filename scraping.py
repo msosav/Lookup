b9b50feaf7705114e8ad380f7f00a9ce11ee82db
miguel jaramillo <migueljaramillo879@gmail.com>
2022-08-15 01:45:08 -0500

    Implementar web scraping mejorado

--- scraping.py
+++ scraping.py
@@ -1,18 +1,35 @@
-from bs4 import BeautifulSoup
-import requests
-import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-url = 'https://www.mercadolibre.com.co/apple-iphone-11-128-gb-negro/p/MCO15149567?pdp_filters=category:MCO1055#reviews-summary'
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
+#location = 'C:\\Users\\user\\Documents\\Proyectos semestre 4\\chromedriver.exe'
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
 
-#Rating
-rating = (soup.find('p', class_="ui-review-view__rating__summary__average")).text
+#Producto
+item = "iphone 11"
 
-#People
-pple = (soup.find('span', class_="ui-pdp-review__amount")).text
-people = pple[pple.index("(")+1 : pple.index(")")]
+#Mercado libre
+driver.get('https://www.mercadolibre.com.co/')
 
+search_box = driver.find_element(by=By.NAME, value="as_word")
+search_box.send_keys(item)
+search_box.send_keys(Keys.RETURN)
 
+product = driver.find_element(By.CLASS_NAME, "ui-search-result-image__element")
+product.click()
 
+review = driver.find_element(By.CLASS_NAME, "ui-pdp-review__ratings")
+review.click()
+
+rating = driver.find_element(By.CLASS_NAME, "ui-review-view__rating__summary__average").text
+people = driver.find_element(By.CLASS_NAME, "ui-review-view__rating__summary__label").text
+
+print(rating)
 print(people)
+
+driver.quit()
