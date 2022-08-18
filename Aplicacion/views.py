db649c078f5758c033581b12c41f22937b80a704
Sergio Córdoba <scordobamuriel@gmail.com>
2022-08-18 12:17:43 -0500

    cambio en analíticas

--- Aplicacion/views.py
+++ Aplicacion/views.py
@@ -11,31 +11,31 @@ collection = db["Aplicacion_producto"]
 
 # Create your views here.
 def inicio(request):
-    #productos = ["Mac", "IPhone 13", "Jordan Retro XI"]
+    analiticas = []
+    for documentos in collection.find():
+        if collection.count_documents({"nombre":documentos["nombre"]}) >= 1:
+            if documentos["nombre"].lower() not in analiticas:
+                analiticas.append(documentos["nombre"])
+    analiticas.sort()
+    for elemento in range(0,len(analiticas)):
+        analiticas[elemento] = analiticas[elemento].capitalize()
+    top_5 = analiticas[0:5]
+
     imagen = "https://m.media-amazon.com/images/I/613AVx005lL._AC_SX522_.jpg"
     caracteristicas = ["Pantalla Super Retina XDR de 6,1 pulgadas",
                         "El modo Cine añade poca profundidad de campo y cambia el enfoque automáticamente en los vídeos", "Sistema avanzado de cámara dual de 12 Mpx con gran angular y ultra gran angular, Estilos Fotográficos, HDR Inteligente 4, modo Noche y grabación de vídeo en 4K HDR con Dolby Vision"]
     if request.method == 'POST':
-        nombre = request.POST.get("producto_buscado")
+        nombre = request.POST.get("producto_buscado").capitalize()
         lista = getMercadoLibre(nombre)
         rating = lista[0]
         precio = lista[1]
         url = lista[2]
-        analiticas = []
         p = Producto(nombre = nombre, price = precio, rating = rating) #Guardar en la base de datos
         p.save()
-        for documentos in collection.find():
-            if collection.count_documents({"nombre":documentos["nombre"]}) >= 1:
-                if documentos["nombre"].lower() not in analiticas:
-                    analiticas.append(documentos["nombre"])
-        analiticas.sort()
-        for elemento in range(0,len(analiticas)):
-            analiticas[elemento] = analiticas[elemento].capitalize()
-        top_5 = analiticas[0:5]
         return render(request, 'inicio.html', {"productos":top_5, "nombre":nombre,
                             "valoracion":rating, "precio":precio, "portal":url,
                             "imagen":imagen, "caracteristicas":caracteristicas})
-    return render(request, 'inicio.html')
+    return render(request, 'inicio.html', {"productos":top_5})
 
 def historial(request):
     return render(request, 'historial.html')
