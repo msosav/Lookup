d4b810ee423e9ebadd85048dfd47fbbf5a90d1d4
Miguel Sosa <miguelsosa2003@gmail.com>
2022-09-05 14:23:02 -0500

    producto recomendado o no (modelo)

--- Aplicacion/models.py
+++ Aplicacion/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+
 class Producto (models.Model):
     nombre = models.CharField(max_length=50)
     price = models.CharField(max_length=50)
     rating = models.CharField(max_length=50)
+    recomendado = models.BooleanField()
--- Aplicacion/views.py
+++ Aplicacion/views.py
@@ -4,41 +4,47 @@ from scraping import getMercadoLibre
 from Aplicacion.models import Producto
 from pymongo import MongoClient
 
-## Se conecta a la base de datos
-cluster = MongoClient("mongodb+srv://sergiocordobam:12345@cluster0.qbfcw.mongodb.net/test")
+# Se conecta a la base de datos
+cluster = MongoClient(
+    "mongodb+srv://sergiocordobam:12345@cluster0.qbfcw.mongodb.net/test")
 db = cluster["lookup"]
 collection = db["Aplicacion_producto"]
 
 # Create your views here.
+
+
 def inicio(request):
     analiticas = []
     for documentos in collection.find():
-        if collection.count_documents({"nombre":documentos["nombre"]}) >= 1:
+        if collection.count_documents({"nombre": documentos["nombre"]}) >= 1:
             if documentos["nombre"].lower() not in analiticas:
                 analiticas.append(documentos["nombre"])
     analiticas.sort()
-    for elemento in range(0,len(analiticas)):
+    for elemento in range(0, len(analiticas)):
         analiticas[elemento] = analiticas[elemento].capitalize()
     top_5 = analiticas[0:5]
 
     imagen = "https://m.media-amazon.com/images/I/613AVx005lL._AC_SX522_.jpg"
     caracteristicas = ["Pantalla Super Retina XDR de 6,1 pulgadas",
-                        "El modo Cine añade poca profundidad de campo y cambia el enfoque automáticamente en los vídeos", "Sistema avanzado de cámara dual de 12 Mpx con gran angular y ultra gran angular, Estilos Fotográficos, HDR Inteligente 4, modo Noche y grabación de vídeo en 4K HDR con Dolby Vision"]
+                       "El modo Cine añade poca profundidad de campo y cambia el enfoque automáticamente en los vídeos", "Sistema avanzado de cámara dual de 12 Mpx con gran angular y ultra gran angular, Estilos Fotográficos, HDR Inteligente 4, modo Noche y grabación de vídeo en 4K HDR con Dolby Vision"]
     if request.method == 'POST':
         nombre = request.POST.get("producto_buscado").capitalize()
         lista = getMercadoLibre(nombre)
         rating = lista[0]
         precio = lista[1]
         url = lista[2]
-        p = Producto(nombre = nombre, price = precio, rating = rating) #Guardar en la base de datos
+        # Guardar en la base de datos
+        p = Producto(nombre=nombre, price=precio, rating=rating)
         p.save()
-        return render(request, 'inicio.html', {"productos":top_5, "nombre":nombre,
-                            "valoracion":rating, "precio":precio, "portal":url,
-                            "imagen":imagen, "caracteristicas":caracteristicas})
-    return render(request, 'inicio.html', {"productos":top_5})
+        return render(request, 'inicio.html', {"productos": top_5, "nombre": nombre,
+                                               "valoracion": rating, "precio": precio, "portal": url,
+                                               "imagen": imagen, "caracteristicas": caracteristicas})
+    return render(request, 'inicio.html', {"productos": top_5})
+
 
 def historial(request):
     return render(request, 'historial.html')
+
 
 def confirmacion(request):
     return render(request, 'confirmacion.html')
