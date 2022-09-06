8bc59364d3f345caa79c04b48b7e2604eec5d03c
Sergio Córdoba <scordobamuriel@gmail.com>
2022-09-06 11:39:37 -0500

    categorización del producto

--- Aplicacion/views.py
+++ Aplicacion/views.py
@@ -1,4 +1,3 @@
-from pickle import FALSE
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from scraping import getMercadoLibre
@@ -12,7 +11,6 @@ db = cluster["lookup"]
 collection = db["Aplicacion_producto"]
 
 # Create your views here.
-
 
 def inicio(request):
     analiticas = []
@@ -34,14 +32,35 @@ def inicio(request):
         rating = lista[0]
         precio = lista[1]
         url = lista[2]
+        electronicos = ["iphone", "samsung", "moto", "hp", "asus"]
+        electrodomesticos = ["televisor", "plancha", "nevera", "ventilador"]
+        hogar = ["silla", "mesa", "cama"]
         if float(rating) < 4.6:
             recomendado = False
         else:
             recomendado = True
-        # Guardar en la base de datos
-        p = Producto(nombre=nombre, price=precio,
-                     rating=rating, recomendado=recomendado)
-        p.save()
+
+        for elemento in range(len(electronicos)):
+            if nombre.lower().__contains__(electronicos[elemento]):
+                categoria_final = "Electronicos"
+                p = Producto(nombre=nombre, price=precio,
+                     rating=rating, recomendado=recomendado, categoria=categoria_final)
+                p.save()
+
+        for elemento in range(len(electrodomesticos)):
+            if nombre.lower().__contains__(electrodomesticos[elemento]):
+                categoria_final = "Electrodomesticos"
+                p = Producto(nombre=nombre, price=precio,
+                     rating=rating, recomendado=recomendado, categoria=categoria_final)
+                p.save()
+        
+        for elemento in range(len(hogar)):
+            if nombre.lower().__contains__(hogar[elemento]):
+                categoria_final = "Hogar"
+                p = Producto(nombre=nombre, price=precio,
+                     rating=rating, recomendado=recomendado, categoria=categoria_final)
+                p.save()
+        
         if (recomendado == False):
             return render(request, 'inicio.html', {"productos": top_5, "nombre2": "funciona",
                                                    "valoracion2": rating, "precio2": precio, "portal2": url,
