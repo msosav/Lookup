33471852a327a67a3b5e958b700848368518cc59
Sergio Córdoba <scordobamuriel@gmail.com>
2022-09-11 20:42:02 -0500

    listas por categoria

--- funciones.py
+++ funciones.py
@@ -15,3 +15,19 @@ def analiticas():
         analiticas[elemento] = analiticas[elemento].capitalize()
     top_5 = analiticas[0:5]
     return top_5
+
+def productosPorCategoria():
+    lista_electronicos = []
+    lista_electrodomesticos = []
+    lista_hogar = []
+    for documentos in collection.find():
+        if documentos["categoria"] == "Electronicos":
+            if documentos["categoria"] not in lista_electronicos:
+                lista_electronicos.append(documentos["nombre"])
+        elif documentos["categoria"] == "Electrodomesticos":
+            if documentos["categoria"] not in lista_electrodomesticos:
+                lista_electrodomesticos.append(documentos["nombre"])
+        elif documentos["categoria"] == "Hogar":
+            if documentos["categoria"] not in lista_hogar:
+                lista_hogar.append(documentos["nombre"])
+    return lista_electronicos, lista_electrodomesticos, lista_hogar
