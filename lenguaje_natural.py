cd4eb82540a09a58ff60078ca4b9eae489a187b8
miguel jaramillo <migueljaramillo879@gmail.com>
2022-10-17 22:42:40 -0500

    Devolver True or False, en vez de un json

--- lenguaje_natural.py
+++ lenguaje_natural.py
@@ -1,8 +1,12 @@
 import requests
 
 def procesamiento(texto: str):
-    r = requests.post(url='https://hf.space/embed/Amrrs/gradio-sentiment-analyzer/+/api/predict/', 
+    r = requests.post(url='https://hf.space/embed/Amrrs/gradio-sentiment-analyzer/+/api/predict/',
     json={"data": [texto]})
-    return r.json()
+    texto = r.text
+    if (texto.__contains__("POSITIVE")):
+        return True
+    else:
+        return False 
 
-print(procesamiento("Buen producto"))
+print(procesamiento(""))
